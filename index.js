91e8aec0ac2332306c76b393d671ee0003e9c784
VictoriaAlekseeva <vicalekseeva88@gmail.com>
2022-12-05 02:54:41 +0100

    feat: add burger menu styles and js to open/close

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+// open/close burger menu
+const burgerMenuButton = document.querySelector('.burger-menu__button');
+const burgerMenu = document.querySelector('.burger-menu');
+const burgerOverlay = document.querySelector('.burger-menu__overlay');
+const navLinks = document.querySelectorAll('.burger-menu__item');
 
+function toggleMenu() {
+    burgerMenu.classList.toggle('burger-menu_active');
+}
+
+function closeMenu() {
+    burgerMenu.classList.remove('burger-menu_active');
+}
+
+
+burgerMenuButton.addEventListener('click', toggleMenu);
+burgerOverlay.addEventListener('click', closeMenu);
+navLinks.forEach((el) => el.addEventListener('click', closeMenu));
